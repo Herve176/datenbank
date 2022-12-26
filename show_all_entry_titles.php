34f2fff7358773503741ee231c09b08171a91006
Nanfack Steve <nanfacksteve7@gmail.com>
2022-12-26 19:58:14 +0100

    Show all bug

--- show_all_entry_titles.php
+++ show_all_entry_titles.php
@@ -46,7 +46,7 @@ if (!isset($_SESSION['idAccount'])) {
         } else {
             /*  build the query string for showing all entry titles grouped by count(*)  
                 using the specialized bug         */
-            $sql = "select * from bug";
+            $sql = "SELECT * FROM bug";
 
             /* perform the query and retrieve a PDOStatement instance */
             $stmt = $dbh->query($sql);
@@ -58,13 +58,15 @@ if (!isset($_SESSION['idAccount'])) {
             include_once("TableBuilder.class.php");
 
             /* create an instance of TableBuilder with widthTable='450px', widthColumn='150px'*/
-            $tb = new TableBuilder('450px', '150px');
+            // $tb = new TableBuilder('450px', '150px');
+            $tb = new TableBuilder('850px', '150px');
 
             /*set background color for table header row */
             $bg_color = 'gold';  //  '#FFFFBB'    #C0C0C0
 
             /*set column names for table header row */
-            $headers = array('entry_title', 'number_of_entries', 'last_entry_date');
+            //$headers = array('entry_title', 'number_of_entries', 'last_entry_date');
+            $headers = array('Summary', 'Description', 'Resolution', 'hours');
 
             /* build the table header row */
             $tb->buildTableHeader($headers, $bg_color);
@@ -79,13 +81,16 @@ if (!isset($_SESSION['idAccount'])) {
 
                 /* use URL rewriting to pass the selected record to the called page  */
                 /* entry_title = via GET submitted variable with the value = $obj->entry_title */
-                $link_address = "page_selected_title_records_02.php?entry_title=" . $obj->entry_title;
-                $link = "<a href='" . $link_address . "'>" . $obj->entry_title . "</a>";
+                // $link_address = "page_selected_title_records_02.php?entry_title=" . $obj->entry_title;
+                // $link = "<a href='" . $link_address . "'>" . $obj->entry_title . "</a>";
+
+                $link_address = "page_selected_title_records.php?entry_title=" . $obj->idBug;
+                $link = "<a href='" . $link_address . "'>" . $obj->summary . "</a>";
 
                 /* fill the $fields array containing also the above constructed hyperlink $link */
                 /* call the method capitalizeName() on the animal instance to show */
                 /* that the result row is actually fetched into an animal's class instance  */
-                $fields = array($link, $obj->number_of_entries, $obj->last_entry_date);
+                $fields = array($link, $obj->description, $obj->resolution, $obj->hours);
 
                 /* format the table rows with alternating colors for better readability */
                 ($odd == TRUE) ? $tb->addRow($fields, $bg_colors[0]) :  $tb->addRow($fields, $bg_colors[1]);
@@ -94,14 +99,14 @@ if (!isset($_SESSION['idAccount'])) {
             }
 
             /* retrieve the whole table and print it */
-            // echo $tb->getTable();
+            //echo $tb->getTable();
 
 
             /* show horizontal line */
             // echo "<br/><hr/><br/>";
 
 
-            $message = 'Show all entry titles';
+            $message = '<br/>Show all Bug';
 
             /* add a context sensitive footer string for the pager footer section */
             $options = array(
